d5c63620f854d1d855527e6e45cf7f010b25681b
Zirno <ziadmontaser2005@gmail.com>
2024-08-15 16:10:15 +0400

    Refactor item controller

---
+++ server/controllers/itemsController.js
@@ -0,0 +1,29 @@
+const Item = require('../../model/Item')
+
+async function handleGetItems(req, res) {
+    const result = await Item.find().exec();
+        res.send(JSON.stringify(result));
+}
+
+async function handleAddItem (req,res){
+
+    if(!req.body.name || !req.body.quantity){
+        return res.status(400).send("Name or quantity is missing.");
+    }
+
+    const newItem = await Item.create(req.body);
+
+    res.status(201).send(newItem)
+}
+
+async function handleUpdateItem (req,res) {
+    const item = await Item.findByIdAndUpdate(req.params.id, req.body);
+    
+    res.send(item);
+}
+
+async function handleDeleteItem (req, res) {
+    await Item.deleteOne({_id: req.params.id})
+}
+
+module.exports = {handleGetItems, handleAddItem, handleUpdateItem, handleDeleteItem};
--- server/routes/api/items.js
+++ server/routes/api/items.js
@@ -2,33 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const Item = require('../../model/Item')
-
+const {handleGetItems, handleAddItem, handleUpdateItem, handleDeleteItem} = require('../../controllers/registerController')
 router.route('/')
-    .get(async (req,res) => {
-        const result = await Item.find().exec();
-        res.send(JSON.stringify(result));
-
-    })
-    .post(async (req,res)=>{
-
-        if(!req.body.name || !req.body.quantity){
-            return res.status(400).send("Name or quantity is missing.");
-        }
-
-        const newItem = await Item.create(req.body);
-
-        res.status(201).send(newItem)
-    });
+    .get(handleGetItems)
+    .post(handleAddItem);
 
 router.route('/:id')
-    .patch( async (req,res)=>{
-        const item = await Item.findByIdAndUpdate(req.params.id, req.body);
-        
-        res.send(item);
-    })
-    .delete( async (req, res) => {
-        await Item.deleteOne({_id: req.params.id})
-    })
-
+    .patch(handleUpdateItem)
+    .delete(handleDeleteItem)
 
 module.exports = router;
